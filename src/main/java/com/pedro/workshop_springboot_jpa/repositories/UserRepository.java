1d6fe94b32e4af43539516c0dbd939c0a7edf40a
ACE638 <pedrogames20.com@gmail.com>
2025-09-05 18:09:38 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/pedro/workshop_springboot_jpa/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pedro.workshop_springboot_jpa.repositories;
+
+import com.pedro.workshop_springboot_jpa.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
